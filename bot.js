192a27d84678e18593784f31ce6e8132add97dbd
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-09-09 17:36:25 +0530

    add /ping route

--- bot.js
+++ bot.js
@@ -112,6 +112,12 @@ controller.middleware.receive.use(dialogflowMiddleware.receive);
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
 
+webserver.get('/ping', (req, res)=>{
+  res.status(200).json({
+    reply : "pong"
+  })
+})
+
 if (!process.env.clientId || !process.env.clientSecret) {
 
   // Load in some helpers that make running Botkit on Glitch.com better
